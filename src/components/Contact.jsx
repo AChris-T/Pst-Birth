21147c48ffef87046182b8333dfbd1c185f45e0f
chris <taiwoakinfenwa2019@gmail.com>
2024-08-16 18:59:46 +0100

    new.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,12 +9,12 @@ const Contact = () => {
   return (
     <div className='flex justify-center items-center flex-col gap-1 w-[100%]'>
       <div className='flex gap-3 flex-col lg:flex-row w-[100%] justify-center items-center'>
-        <div data-aos="fade-right"  data-aos-duration="3000" className='nani gap-3 text-white flex-col w-[90%] lg:w-[500px] h-[300px] flex justify-center items-center'>
+        <div data-aos="fade-right"  data-aos-duration="3000" className='nani gap-3 text-white font-bold flex-col w-[90%] lg:w-[500px] h-[300px] flex justify-center items-center'>
             <h3 className='text-[30px] text-center'>For Details on Aso Ebi</h3>
             <h3 className='flex text-center'>Contact Sis Jessica E.Ohwevwo @ 3473018796</h3>
             <button className='rounded-full border-[1px] px-5 py-2 hover:bg-[#de603a] hover:text-[white]'>Contact Us</button>
         </div>
-        <div data-aos="fade-left"  data-aos-duration="3000" className=' nan gap-3 text-white flex-col w-[90%] lg:w-[500px] h-[300px] flex justify-center items-center'>
+        <div data-aos="fade-left"  data-aos-duration="3000" className=' nan gap-3 text-white font-bold flex-col w-[90%] lg:w-[500px] h-[300px] flex justify-center items-center'>
         <h3 className='text-[30px]'>RSVP For Seat</h3>
             <h3>Pastor Yetty +1(646) 314-1169</h3>
             <h3>Pastor Sola +1(347) 940-2752</h3>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useEffect } from "react";
 import cel from "../assets/item1.png"
 import item from "../assets/item2.png"
@@ -35,19 +36,18 @@ const HeroSection = () => {
           }
         } catch (error) {
           console.error('Checkout error:', error);
-        }
+        }                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
       };
   return (
     <>
-    <div className="hero flex justify-around items-center text-white gap-2">
-    <div className="hidden md:block"  data-aos="fade-right" data-aos-delay="900"  data-aos-duration="3000">
+    <div className="hero flex justify-around items-center text-white gap-2"><div className="hidden md:block"  data-aos="fade-right" data-aos-delay="900"  data-aos-duration="3000">
     <img src={cel} alt="cel"/>
     </div>
     <div className="flex items-center flex-col gap-2">
     <img  data-aos="fade-right"  data-aos-duration="3000" src={sweet} alt="cel"/>
     <h3  data-aos="fade-left"  data-aos-duration="3000" className="text-[30px] text-center md:text-[40px] font-bold"> Celebrating the Life of Pastor Daniel Adeniran</h3>
         <h4  data-aos="fade-right"  data-aos-duration="3000" className=" text-[20px] text-center md:text-[30px] font-semibold">Join us in honoring 60 years of God's faithfulness</h4>
-        <a  data-aos="fade-in" onClick={handleCheckout}  data-aos-duration="3000" className="bg-[#de603a] w-36 h-12 flex justify-center items-center rounded cursor-pointer mt-4">RSVP Now</a>
+        <a  data-aos="fade-in" target="_blank" onClick={handleCheckout}  data-aos-duration="3000" className="bg-[#de603a] w-36 h-12 flex justify-center items-center rounded cursor-pointer mt-4">RSVP Now</a>
     </div>
     <div  className="hidden md:block" data-aos="fade-left" data-aos-delay="900"  data-aos-duration="3000">
     <img src={item} alt="cel"/>
